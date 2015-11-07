5ef98eecb9f0d8baba30e811ccb3d584dca66fc1
Kulbir Saini <saini@saini.co.in>
2015-11-07 15:49:27 +0530

    Added twitter, vine, instagram support.

--- web/js/api/index.js
+++ web/js/api/index.js
@@ -18,11 +18,32 @@ export function GET(url, data = null) {
   if (data) {
     url += '?' + Qs.stringify(data);
   }
-  return fetch(url, { credentials: 'Access-Control-Allow-Origin' })
+  return fetch(url, { headers: { 'Access-Control-Allow-Origin': '*' } })
     .then(errorFilter)
     .then((response) => response.json());
 }
 
+export function corsGET(url, data = null) {
+  const callback = `callback_${Date.now()}`;
+
+  return new Promise(function(resolve, reject) {
+    window[callback] = function (data) {
+      delete window[callback];
+      resolve(data);
+    };
+
+    url += '?' + Qs.stringify(Object.assign({}, data || {}, { callback: callback }));
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.head.appendChild(script);
+  });
+}
+
 export function getTweetJson(tweet_url) {
   return GET('https://api.twitter.com/1/statuses/oembed.json', { url: tweet_url });
+}
+
+export function getInstagramJson(url) {
+  return corsGET('http://api.instagram.com/oembed', { url, omitscript: true });
 }
