52f29cc2c6fbfa6be82545d075cddf8f59091ca8
Kulbir Saini <saini@saini.co.in>
2015-11-07 23:36:20 +0530

    Added imgur support

---
+++ web/js/utils/imgur.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import { findElementIdInCards, parseUrl } from './common';
+
+export function createImgurEmbed(imgId, cards) {
+  const elementId = 'imgur-embed-' + imgId;
+  if (findElementIdInCards(elementId, cards)) {
+    return Promise.reject(new Error('Card already exists'));
+  }
+  const render = () => {
+    document.getElementById(elementId).innerHTML = `<blockquote class="imgur-embed-pub" lang="en" data-id="${imgId}"></blockquote>`;
+    imgurEmbed.createIframe();
+  };
+  return { type: 'imgur', elementId, render };
+}
+
+export function getImgurCard(text, type, cards) {
+  if (type === 'block') {
+    const matches = text.match(/\sdata-id="([a-z0-9_\-\/]+)"/i);
+    if (!matches || !matches[1]) {
+      return null;
+    }
+    return createImgurEmbed(matches[1], cards);
+  }
+
+  if (type === 'url') {
+    const parsedUrl = parseUrl(text);
+    const pathname = parsedUrl.pathname;
+
+    let matches = pathname.match(/\/a\/([a-z0-9_\-]+)$/i);
+    if (matches && matches.length > 1 && matches[1]) {
+      return createImgurEmbed('a/' + matches[1], cards);
+    }
+
+    matches = pathname.match(/\/([a-z0-9_\-]+)$/i);
+    if (matches && matches.length > 1 && matches[1]) {
+      return createImgurEmbed(matches[1], cards);
+    }
+    return null;
+  }
+}
