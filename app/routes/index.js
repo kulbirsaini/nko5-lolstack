f683eb80442f1fc52a05360d72bd1f902ee9af8c
Kulbir Saini <saini@saini.co.in>
2015-11-09 01:38:55 +0530

    Bug fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,17 @@ const router = require('express').Router();
 
 const Middlewares = require(path.join(__dirname, '../middlewares'));
 
+function getCurrentUserId() {
+  try {
+    return req._currentUser.getProp('id');
+  } catch (error) {
+    return null;
+  }
+}
+
 router.get('/', function(req, res) {
   if (req._currentUser) {
-    return res.render('pages/home', { current_user: req._currentUser.getProp('id') });
+    return res.render('pages/home', { current_user: getCurrentUserId() });
   } else {
     return res.render('pages/login');
   }
@@ -17,13 +25,13 @@ router.use('/boards/:board_id', Middlewares.setCurrentBoard);
 
 router.get('/boards/:board_id', function(req, res, next) {
   if (req._currentBoard) {
-    return res.render('pages/board', { current_user: req._currentUser.getProp('id') });
+    return res.render('pages/board', { current_user: getCurrentUserId() });
   }
   next();
 });
 
 router.get('/boards', function(req, res) {
-  return res.render('pages/boards', { current_user: req._currentUser.getProp('id') });
+  return res.render('pages/boards', { current_user: getCurrentUserId() });
 });
 
 module.exports = router;
