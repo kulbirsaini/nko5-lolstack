2cc0bbbfb286f56ad9e48eecc2e0168e3cc5a32e
Kulbir Saini <saini@saini.co.in>
2015-11-09 01:27:28 +0530

    Added logout handler

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,18 +3,22 @@
 const passport = require('passport');
 const router   = require('express').Router();
 
+function logout(req, res) {
+  req.session.destroy();
+  return res.redirect("/");
+}
+
 router.get('/twitter', passport.authenticate('twitter'));
 
 router.get('/twitter/callback',
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
-    return res.redirect('/');
+    res.redirect('/');
   });
 
 
-router.get('/logout', function(req, res) {
-  req.session.destroy();
-  return res.redirect("/");
-});
+router.get('/logout', logout);
+router.delete('/logout', logout);
+router.post('/logout', logout);
 
 module.exports = router;
---
+++ web/js/components/logout.jsx
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+import { RaisedButton } from 'material-ui';
+
+import { logout } from '../api';
+
+export default class Logout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutHandler = this.logoutHandler.bind(this);
+  }
+
+  logoutHandler() {
+    window.location.href = '/auth/logout';
+  }
+
+  render() {
+    return (
+      <RaisedButton link={true} primary={true} label="Logout" onClick={this.logoutHandler} />
+    );
+  }
+}
